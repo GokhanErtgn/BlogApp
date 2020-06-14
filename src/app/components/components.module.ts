536d52f1603fab70fb7103a5df174a7d13056e41
Gokhan Ertogan <gokhanertogan@gmail.com>
2020-06-14 17:30:44 +0300

    abc

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {NgxPaginationModule} from 'ngx-pagination';
+
 import {MenuCategoryComponent} from './menu-category/menu-category.component';
 import { PageTitleComponent } from './page-title/page-title.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
-  declarations: [MenuCategoryComponent, PageTitleComponent],
+  declarations: [MenuCategoryComponent, PageTitleComponent,ArticlesComponent],
   imports: [
-    CommonModule,RouterModule
+    CommonModule,RouterModule, NgxPaginationModule
   ],
   exports:[
-      MenuCategoryComponent,PageTitleComponent
+      MenuCategoryComponent,PageTitleComponent,ArticlesComponent
   ]
 })
 export class ComponentsModule { }
